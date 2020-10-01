9a3196ccebd921889af9742ac99c77e993f4d8b7
ksxjltze <leejiakeat@gmail.com>
2020-10-01 19:19:05 +0800

    Fixed player initilalization code.

--- CProcessing/game.c
+++ CProcessing/game.c
@@ -46,7 +46,7 @@ void game_init(void)
 void init_entities()
 {
 	//Player
-	init_player();
+	init_player(player, player_width, player_height);
 
 	//TODO: Possibly implement an entity manager to manage different types of entities.
 	init_enemies(arr_enemy, sizeof(arr_enemy) / sizeof(arr_enemy[0]), enemy_width, enemy_height);
--- CProcessing/player.h
+++ CProcessing/player.h
@@ -4,9 +4,6 @@
 #include "collider_aabb.h"
 #include "status.h"
 
-void init_player();
-void draw_player(CP_Image player_sprite, CP_Vector pos, float player_width, float player_height, float player_rotation);
-
 struct Player {
 	CP_Vector pos;
 	struct Health hp;
@@ -14,3 +11,7 @@ struct Player {
 	struct Status status;
 	int active;
 };
+
+void init_player(struct Player player, float player_width, float player_height);
+void draw_player(CP_Image player_sprite, CP_Vector pos, float player_width, float player_height, float player_rotation);
+
