8503a7f3cae3854dab2ae74b3a3b85511054003e
dania <yuukidania@gmail.com>
2020-11-23 10:07:50 +0800

    Animation for explosion cleanup

--- src/asteroids/game.c
+++ src/asteroids/game.c
@@ -61,7 +61,7 @@ void Asteroids_Init(void)
 	Asteroids_Entities_Init();
 	Asteroids_Pause_Init();
 
-	particle_init();
+	explosion_init();
 	Asteroids_Init_Powerups();
 	Asteroids_Init_Score();
 	Asteroids_Powerup_Player_Interaction_Init();
