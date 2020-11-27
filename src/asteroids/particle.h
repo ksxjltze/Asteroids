11e4c545ecac970dd4b118673aab24c2d895c7c2
dania <yuukidania@gmail.com>
2020-11-27 15:13:40 +0800

    Smoke Anim fixed

--- src/asteroids/particle.h
+++ src/asteroids/particle.h
@@ -24,13 +24,15 @@ void explosion_init(void);
 void smoke_init(void);
 
 void spawn_explosion_anim(CP_Vector position, float size);
-void spawn_smoke_trail_anim(CP_Vector position, float size, CP_Vector rotation);
+void spawn_smoke_trail_anim(CP_Vector position, float size);
 
 void particle_update(void);
 void draw_particle();
 
-void Spawn_Particle(CP_Vector position, int particles, float min_velocity, float max_velocity, float size, Sprite sprite, int type, bool loop);
+Particle* Spawn_Particle(CP_Vector position, int particles, float min_velocity, float max_velocity, float size, Sprite sprite, int type, bool loop);
 
 void smoke_velocity(CP_Vector position, int particles, float min_velocity, float max_velocity, float size);
 
 void particle_despawning(Particle* particle);
+
+void smoke_update(CP_Vector rotation, CP_Vector playerPos);
--- src/asteroids/sprite.c
+++ src/asteroids/sprite.c
@@ -6,8 +6,8 @@ Sprite Asteroids_Sprite_Create(CP_Image images[], CP_Vector dimensions[], int fr
 	sprite.images = images;
 	sprite.dimensions = dimensions;
 	sprite.frame_count = frame_count;
-	sprite.duration = duration;
+	sprite.duration = duration; //entire particle effect lifetime
 	sprite.keyframe = start_frame;
-	sprite.time = duration / frame_count;
+	sprite.time = duration / frame_count; //lifetime of one frame
 	return sprite;
 }
