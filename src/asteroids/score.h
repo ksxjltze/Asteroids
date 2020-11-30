7a968bdd30cc3ae6ce84d1cd39f1c01ca51effc7
ksxjltze <leejiakeat@gmail.com>
2020-11-30 17:07:24 +0800

    insert again

--- src/asteroids/score.h
+++ src/asteroids/score.h
@@ -25,6 +25,3 @@ void Asteroids_Draw_Scores(void);
 // SCORE MANAGERS
 void Asteroids_EnemyKill_Score_Manager(void);
 void Asteroids_Time_Score_Manager(void);
-
-//void Asteroids_Pause_Timer(void);
-//void Asteroids_Resume_Timer(void);
