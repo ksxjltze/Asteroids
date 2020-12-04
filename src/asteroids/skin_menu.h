a0c3d2693928ab57730cade5d9d082e0fff3b9d0
ksxjltze <leejiakeat@gmail.com>
2020-12-04 17:36:00 +0800

    skin menu file level doc

--- src/asteroids/skin_menu.h
+++ src/asteroids/skin_menu.h
@@ -1,3 +1,15 @@
+//---------------------------------------------------------
+// file:	skin_menu.h
+// author:	Lee Jia Keat
+// contributors: Liu Ke (Additional Skins)
+// email:	l.jiakeat@digipen.edu, 
+//
+// brief:	Skin menu header file.
+//			Handles Initialization, Loading, Selecting and Saving of skins.
+//			Reads and Writes from a specific file to read and save player skins.
+//			Displays a user interface for swapping skins.
+// Copyright  2020 DigiPen, All rights reserved.
+//---------------------------------------------------------
 #pragma once
 #include "cprocessing.h"
 
