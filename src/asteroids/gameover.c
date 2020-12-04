a8520991947b51a370bc37450fda23b7b626a1d4
ksxjltze <leejiakeat@gmail.com>
2020-12-04 14:46:52 +0800

    wut

--- src/asteroids/gameover.c
+++ src/asteroids/gameover.c
@@ -145,7 +145,6 @@ void Asteroids_GameOver_Init_Score(void)
 {
 	sprintf_s(str_time_score, 20, "Time: %.2fs", CURRENT_SCORE.time_score);
 	sprintf_s(str_kill_score, 20, "Destroyed: %d", CURRENT_SCORE.enemy_kill_score);
-	Asteroids_Leaderboard_Insert_Score(CURRENT_SCORE);
 }
 
 void Asteroids_GameOver_Display_Score(void)
