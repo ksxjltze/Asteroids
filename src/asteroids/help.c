8d545809c02c2230585d6da2e0fe330200ac594f
bryan <usernamee19@gmail.com>
2020-12-04 10:22:15 +0800

    more work done for help page

--- src/asteroids/help.c
+++ src/asteroids/help.c
@@ -1,6 +1,7 @@
 #include "help.h"
 #include "utility.h"
 #include "button.h"
+#include "main_menu.h"
 
 #define BUTTON_WIDTH 200
 #define BUTTON_HEIGHT 100
@@ -13,7 +14,7 @@ enum Page_Num {PAGE_1 = 1, PAGE_2, PAGE_3, PAGE_4 };
 
 CP_Image Page_Image[LAST_PAGE];
 
-Button Back, Next, Exit;
+Button BackBtn, NextBtn, ExitBtn;
 
 void Asteroids_Help_Screen_Init(void)
 {
@@ -26,15 +27,14 @@ void Asteroids_Help_Screen_Init(void)
 	screen.width = (float)CP_System_GetWindowWidth();
 	screen.height = (float)CP_System_GetWindowHeight();
 	screen.pos = Asteroids_Utility_GetWindowMiddle();
-	screen.active = false;
 	screen.Image = Page_Image[screen.id];
 
 
 	Asteroids_Button_Add_New_Button(BUTTON_WIDTH, BUTTON_HEIGHT);
 
-	Asteroids_Button_Set_Text(&Back, BTN_TEXTSIZE, "Back");
-	Asteroids_Button_Set_Text(&Next, BTN_TEXTSIZE, "Next");
-	Asteroids_Button_Set_Text(&Exit, BTN_TEXTSIZE, "Exit");
+	Asteroids_Button_Set_Text(&BackBtn, BTN_TEXTSIZE, "Back");
+	Asteroids_Button_Set_Text(&NextBtn, BTN_TEXTSIZE, "Next");
+	Asteroids_Button_Set_Text(&ExitBtn, BTN_TEXTSIZE, "Exit");
 
 	CP_Vector position_offset = Asteroids_Utility_GetWindowMiddle();
 
@@ -42,42 +42,49 @@ void Asteroids_Help_Screen_Init(void)
 	CP_Vector ExitBtnPos = CP_Vector_Set(position_offset.x - BUTTON_WIDTH / 2, (float)CP_System_GetWindowHeight() - BUTTON_HEIGHT / 2);
 	CP_Vector NextBtnPos = CP_Vector_Set((float)CP_System_GetWindowWidth() - BUTTON_WIDTH / 2, position_offset.y - BUTTON_HEIGHT / 2);
 
-	Asteroids_Button_Set_Position(&Back, BackBtnPos);
-	Asteroids_Button_Set_Position(&Exit, ExitBtnPos);
-	Asteroids_Button_Set_Position(&Next, NextBtnPos);
+	Asteroids_Button_Set_Position(&BackBtn, BackBtnPos);
+	Asteroids_Button_Set_Position(&NextBtn, NextBtnPos);
+	Asteroids_Button_Set_Position(&ExitBtn, ExitBtnPos);
+
+	Asteroids_Button_Set_Callback_Button(Asteroids_Help_Screen_Exit_ToMenu, &ExitBtn);
+	Asteroids_Button_Set_Callback_Button(Asteroids_Help_Screen_IncreasePageNo, &NextBtn);
+	Asteroids_Button_Set_Callback_Button(Asteroids_Help_Screen_DecreasePageNo, &BackBtn);
+
+
 }
 void Asteroids_Help_Screen_Update(void)
 {
+	if (screen.id != PAGE_1)
+		Asteroids_Button_Update(&BackBtn);
+	if (screen.id != LAST_PAGE)
+		Asteroids_Button_Update(&NextBtn);
 
+	Asteroids_Draw_Screen_Page();
+	Asteroids_Help_Screen_UpdatePageNo();
+	Asteroids_Button_Update(&ExitBtn);
 }
 void Asteroids_Help_Screen_Exit(void)
 {
 
 }
 
+void Asteroids_Help_Screen_Exit_ToMenu(void)
+{
+	CP_Engine_SetNextGameState(Asteroids_MainMenu_Init, Asteroids_MainMenu_Update, Asteroids_MainMenu_Exit);
+}
+
 void Asteroids_Draw_Screen_Page(void)
 {
+
 	CP_Image_Draw(Page_Image[screen.id], screen.pos.x, screen.pos.y, screen.width, screen.height, 255);
 }
 
-int Asteroids_Help_Screen_CheckPageNo(void)
+void Asteroids_Help_Screen_IncreasePageNo(void)
 {
-	switch (screen.id)
-	{
-	case PAGE_1:
-		return PAGE_1;
+	screen.id += 1;
+}
 
-	case PAGE_2:
-		return PAGE_2;
-
-	case PAGE_3:
-		return PAGE_3;
-
-	case PAGE_4:
-		return PAGE_4;
-
-	default:
-		break;
-	}
-	return 0;
+void Asteroids_Help_Screen_DecreasePageNo(void)
+{
+	screen.id -= 1;
 }
--- src/asteroids/help.h
+++ src/asteroids/help.h
@@ -7,15 +7,16 @@ typedef struct Help_Screen
 	int id;
 	CP_Vector pos;
 	CP_Image Image;
-	bool active;
 	float width, height;
 }Help_screen;
 
 void Asteroids_Help_Screen_Init(void);
 void Asteroids_Help_Screen_Update(void);
 void Asteroids_Help_Screen_Exit(void);
+void Asteroids_Help_Screen_Exit_ToMenu(void);
 
 void Asteroids_Draw_Screen_Page(void);
 
-int Asteroids_Help_Screen_CheckPageNo(void);
-void Asteroids_Help_Screen_CheckInput(void);
+void Asteroids_Help_Screen_IncreasePageNo(void);
+
+void Asteroids_Help_Screen_DecreasePageNo(void);
